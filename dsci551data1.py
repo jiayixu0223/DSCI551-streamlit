ed2916aa0b9f9dac6d92bcd5b3686198a9472c15
jiayixu0223 <104292588+jiayixu0223@users.noreply.github.com>
2022-04-25 20:04:30 -0700

    Delete dsci551data1.py

--- dsci551data1.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import requests
-url = 'https://dsci551-project-data1-default-rtdb.firebaseio.com/.json'
-response = requests.get(url)
-response1 = response.json()
-df1 = pd.DataFrame.from_dict(response1)
-st.dataframe(df1)
